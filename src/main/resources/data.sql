1e0ab36ed93dfb2cae2cda80b8a118992931bd86
Rodrigo Pereyra <rodrigopereyra02@gmail.com>
2023-10-15 23:29:23 -0300

    .

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,14 @@
-INSERT INTO books (id,name,author,edition_date, code_book) VALUES (1001,'HARRY POTTER','NO SABEMOS','2023-12-12','978987');
-INSERT INTO books (id,name,author,edition_date, code_book) VALUES (1002,'Los vengadores','Marvel','2023-12-12','978987');
+INSERT INTO books (id,name,author,edition_date, code_book) VALUES (1001,'El tunel','Ernesto Sábato','1948-03-25','9789500433624');
+INSERT INTO books (id,name,author,edition_date, code_book) VALUES (1002,'Martin Fierro','Jose Hernandez','1872-08-12','987590004');
+INSERT INTO books (id,name,author,edition_date, code_book) VALUES (1003,'El Aleph','Jorge Luis Borges','1945-06-1','9789500755764');
+INSERT INTO books (id,name,author,edition_date, code_book) VALUES (1004,'Operacion Masacre','Rodolfo Wash','1957-07-1','9789505153527');
+INSERT INTO books (id,name,author,edition_date, code_book) VALUES (1005,'Rayuela','Julio Cortazar','1963-02-14','9788437604572');
+INSERT INTO books (id,name,author,edition_date, code_book) VALUES (1006,'Ficciones','Jorge Luis Borges','1944-04-24','9780802130303');
+INSERT INTO books (id,name,author,edition_date, code_book) VALUES (1007,'Cuentos de la selva','Horacio Quiroga','1918-09-23','9789505810758');
+INSERT INTO books (id,name,author,edition_date, code_book) VALUES (1008,'Las fieras cómplices','Horacio Quiroga','2007-10-24','9788496509740');
+INSERT INTO books (id,name,author,edition_date, code_book) VALUES (1009,'Fahrenheit 451','Ray Bradbury','1953-01-24','9788445006771');
+
+
+
+
+
